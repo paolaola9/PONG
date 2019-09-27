7a869c6550cdbaa6398ff5b38f5c350f47052611
AlejoQT <agqt09@gmail.com>
2019-09-27 04:07:03 -0500

    pong

---
+++ practica/Assets/Scenes/BALLCONTROL.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BALLCONTROL : MonoBehaviour {
+    int speddX;
+    int speddY;
+
+    float spedd;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        spedd=Random.Range(5,10);
+
+        speddX=Random.Range(0,2);
+        if(speddX==0){
+            speddX=1;
+        } else{
+            speddX=-1;
+        }
+
+        speddY=Random.Range(0,2);
+                if(speddY==0){
+                    speddY=1;
+                } else{
+                    speddY=-1;
+                }
+        GetComponent<Rigidbody>().velocity= new Vector3(spedd*speddX,spedd*speddY,0);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ practica/Assets/Scenes/PLAYER.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYER : MonoBehaviour
+{
+    Public KeyCode up;
+    Public KeyCode down;
+
+    float spedd;
+
+    // Start is called before the first frame update
+    void Start() {
+        spedd=0.15f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.Getkey(up)){
+            transform.Translate(0,spedd,0);
+         }
+         if (Input.GetKey(down)){
+          transform.Translate(0,-spedd,0);
+         }
+        }
+      }
